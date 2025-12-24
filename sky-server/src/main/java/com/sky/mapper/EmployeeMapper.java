d5fe1bf2e6b7da62e1b2929cb7c6cbacc44b4613
马欣怡 <2366614302@qq.com>
2025-12-24 16:54:29 +0800

    编辑员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -26,4 +26,8 @@ public interface EmployeeMapper {
 
 
     void update(Employee employee);
+
+    //根据id查询员工信息
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
